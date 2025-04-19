5ef1aacaec28c0a91c7a8a2862df566e5422cf8c
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2025-04-19 23:34:34 +0900

    font 추가

--- composeApp/src/commonMain/kotlin/com/msg/onmip/App.kt
+++ composeApp/src/commonMain/kotlin/com/msg/onmip/App.kt
@@ -10,11 +10,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import onmip.composeapp.generated.resources.Pretendard_Bold
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import onmip.composeapp.generated.resources.Res
 import onmip.composeapp.generated.resources.compose_multiplatform
+import org.jetbrains.compose.resources.Font
 
 @Composable
 @Preview
@@ -27,9 +30,16 @@ fun App() {
             }
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                Column(
+                    Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
                     Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+                    Text(
+                        "Compose: $greeting", fontFamily = FontFamily(
+                            Font(Res.font.Pretendard_Bold)
+                        )
+                    )
                 }
             }
         }
