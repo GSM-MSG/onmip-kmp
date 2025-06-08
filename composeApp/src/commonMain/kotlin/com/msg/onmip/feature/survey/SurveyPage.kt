0dad045fdd1ff8d716dd65e417e9a70ce790494d
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2025-06-08 16:44:58 +0900

    survey page 추가

---
+++ composeApp/src/commonMain/kotlin/com/msg/onmip/feature/survey/SurveyPage.kt
@@ -0,0 +1,13 @@
+package com.msg.onmip.feature.survey
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SurveyPages(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxSize()) {
+
+    }
+}
