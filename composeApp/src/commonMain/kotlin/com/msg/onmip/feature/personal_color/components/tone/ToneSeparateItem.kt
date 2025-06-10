15680391187aba7afc9287bc7bbae8a36a35dfd9
khs3994 <dev.hyunsss@gmail.com>
2025-06-10 23:47:30 +0900

    [Add] :: 웜/쿨 톤 구분 섹션아이템 선택 가능하도록 추가 & 선택된 상태에서의 UI 추가

--- composeApp/src/commonMain/kotlin/com/msg/onmip/feature/personal_color/components/tone/ToneSeparateItem.kt
+++ composeApp/src/commonMain/kotlin/com/msg/onmip/feature/personal_color/components/tone/ToneSeparateItem.kt
@@ -1,16 +1,23 @@
 package com.msg.onmip.feature.personal_color.components.tone
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -19,31 +26,68 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.msg.onmip.shared.ui.theme.color.Black
+import com.msg.onmip.shared.ui.theme.color.Green
 import com.msg.onmip.shared.ui.theme.typography.AppFont
+import onmip.composeapp.generated.resources.Res
+import onmip.composeapp.generated.resources.ic_green_check
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun ToneSeparateItem(
     modifier: Modifier = Modifier,
+    selected: Boolean,
     color: Color,
     description: String,
+    onClick: () -> Unit,
 ) {
-    Column {
-        Box(
-            modifier = modifier
-                .fillMaxWidth()
-                .aspectRatio(1f)
-                .clip(RoundedCornerShape(12.dp))
-                .background(color)
-        )
-        Spacer(modifier = Modifier.height(12.dp))
-        Text(
-            text = description,
-            style = TextStyle(
-                fontFamily = AppFont(),
-                color = Black,
-                fontSize = 16.sp,
-                fontWeight = FontWeight.Bold
+    Box {
+        Column {
+            Box(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f)
+                    .clip(RoundedCornerShape(12.dp))
+                    .background(color)
+                    .clickable(onClick = onClick)
             )
+            Spacer(modifier = Modifier.height(12.dp))
+            Text(
+                text = description,
+                style = TextStyle(
+                    fontFamily = AppFont(),
+                    color = Black,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+        }
+    }
+
+    AnimatedVisibility(
+        visible = selected,
+        enter = fadeIn(),
+        exit = fadeOut()
+    ) {
+        SelectedBoxCover()
+    }
+}
+
+@Composable
+fun SelectedBoxCover() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(1f)
+            .clip(RoundedCornerShape(12.dp))
+            .border(3.dp, Green, RoundedCornerShape(12.dp))
+            .background(Black.copy(alpha = 0.2f))
+    ) {
+        Image(
+            modifier = Modifier
+                .padding(10.dp)
+                .align(Alignment.TopEnd),
+            painter = painterResource(Res.drawable.ic_green_check),
+            contentDescription = null
         )
     }
 }
--- composeApp/src/commonMain/kotlin/com/msg/onmip/feature/personal_color/components/tone/ToneSeparateSection.kt
+++ composeApp/src/commonMain/kotlin/com/msg/onmip/feature/personal_color/components/tone/ToneSeparateSection.kt
@@ -8,7 +8,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -19,6 +24,10 @@ fun ToneSeparateSection(
     title: String,
     colors: List<Pair<Color, String>>,
 ) {
+    var selectedIndex by remember {
+        mutableIntStateOf(-1)
+    }
+
     Column(modifier = modifier.fillMaxWidth()) {
         ToneSeparateSectionHeader(title = title)
         Spacer(modifier = Modifier.height(24.dp))
@@ -27,10 +36,14 @@ fun ToneSeparateSection(
             verticalArrangement = Arrangement.spacedBy(12.dp),
             horizontalArrangement = Arrangement.spacedBy(12.dp)
         ) {
-            items(colors) { item ->
+            itemsIndexed(colors) { index, item ->
                 ToneSeparateItem(
                     color = item.first,
-                    description = item.second
+                    description = item.second,
+                    selected = index == selectedIndex,
+                    onClick = {
+                        selectedIndex = if (selectedIndex == index) -1 else index
+                    }
                 )
             }
         }
